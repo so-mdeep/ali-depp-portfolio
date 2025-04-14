18732e51b8afcf3efab8c63f9e100dbb92786578
so-mdeep <so22mdeep@gmail.com>
2025-04-15 00:05:36 +0200

    Add files via upload

---
+++ assets/js/Lotti Holder.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var animation = lottie.loadAnimation({
+        container: document.getElementById('lottie-container'), 
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '../Wapay Processing Animation IOS'
+    });
+});
